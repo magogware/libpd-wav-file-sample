77d9c0b73199d9029b0945c2cbad5da46d51e710
Alex Barilaro <alex@barilaro.me>
2022-02-22 19:15:56 +1100

    Initialise patch correctly

--- sample.c
+++ sample.c
@@ -13,9 +13,14 @@ int main(int argc, char **argv) {
     if(!libpd_openfile("sawtooth.pd", "./"))
         return -1;
     f = wavfile_open("sound.wav");
+    
+    libpd_start_message(1); // one entry in list
+    libpd_add_float(1.0f);
+    libpd_finish_message("pd", "dsp");
 
     for (i = 0; i < 4 * WAVFILE_SAMPLES_PER_SECOND / 64; i++) {
         libpd_process_float(1, NULL, outbuf);
+        printf("%f\n", outbuf[0]);
         wavfile_write(f,(short *) outbuf,128);
     }
     wavfile_close(f);
